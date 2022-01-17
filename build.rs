d0a1a09f02f3e66286dd387d84965c8bdfc54286
ljoonal <ljoonal@ljoonal.xyz>
2022-01-18 01:32:09 +0200

    Fix windows build icon

--- build.rs
+++ build.rs
@@ -1,9 +1,17 @@
-#[cfg(windows)]
-fn main() {
-	let mut res = winres::WindowsResource::new();
-	res.set_icon("static/logo.ico");
-	res.compile().unwrap();
-}
+use std::env;
 
-#[cfg(unix)]
-fn main() {}
+fn main() {
+	if env::var("PROFILE").unwrap() == "release"
+		&& env::var("CARGO_CFG_WINDOWS").is_ok()
+	{
+		let mut res = winres::WindowsResource::new();
+		if let Ok(host) = env::var("HOST") {
+			if host.contains("linux") {
+				res.set_toolkit_path("/usr/bin")
+					.set_windres_path("x86_64-w64-mingw32-windres");
+			}
+		}
+		res.set_icon("static/logo.ico");
+		res.compile().unwrap();
+	}
+}
