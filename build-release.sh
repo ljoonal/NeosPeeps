65567841fab3334c9ca10aabaa02ffb5d1102601
ljoonal <ljoonal@ljoonal.xyz>
2022-01-20 23:54:15 +0200

    Make sha256sums relative to current dir

--- build-release.sh
+++ build-release.sh
@@ -4,10 +4,10 @@ set -e
 # Build for linux
 cargo +stable build --release && strip target/release/neos_peeps && sstrip target/release/neos_peeps
 # Create sha256 integrity hash
-sha256sum target/release/neos_peeps > target/release/neos_peeps.sha256
+cd target/release && sha256sum neos_peeps > neos_peeps.sha256 && cd ../..
 
 # Build for windows
 cargo +stable build --release --target x86_64-pc-windows-gnu && strip target/x86_64-pc-windows-gnu/release/neos_peeps.exe
 
 # Create sha256 integrity hash
-sha256sum target/x86_64-pc-windows-gnu/release/neos_peeps.exe > target/x86_64-pc-windows-gnu/release/neos_peeps.exe.sha256
+cd target/x86_64-pc-windows-gnu/release && sha256sum neos_peeps.exe > neos_peeps.exe.sha256 && cd ../../..
