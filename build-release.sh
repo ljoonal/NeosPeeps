7ddf9b4a2c079b9bfbe53b903e386d6635b8bfbf
ljoonal <ljoonal@ljoonal.xyz>
2022-01-20 11:47:05 +0200

    Make sure that releases are built on stable rust

--- build-release.sh
+++ build-release.sh
@@ -2,12 +2,12 @@
 set -e
 
 # Build for linux
-cargo build --release && strip target/release/neos_peeps && sstrip target/release/neos_peeps
+cargo +stable build --release && strip target/release/neos_peeps && sstrip target/release/neos_peeps
 # Create sha256 integrity hash
 sha256sum target/release/neos_peeps > target/release/neos_peeps.sha256
 
 # Build for windows
-cargo build --release --target x86_64-pc-windows-gnu && strip target/x86_64-pc-windows-gnu/release/neos_peeps.exe
+cargo +stable build --release --target x86_64-pc-windows-gnu && strip target/x86_64-pc-windows-gnu/release/neos_peeps.exe
 
 # Create sha256 integrity hash
 sha256sum target/x86_64-pc-windows-gnu/release/neos_peeps.exe > target/x86_64-pc-windows-gnu/release/neos_peeps.exe.sha256
