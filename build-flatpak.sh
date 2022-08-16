ac836682060a677e769bbc39c1594c31a6c30525
ljoonal <ljoonal@ljoonal.xyz>
2022-08-16 21:11:19 +0300

    More flatpak/hub progress

--- build-flatpak.sh
+++ build-flatpak.sh
@@ -3,5 +3,7 @@ set -e
 
 # Get current version
 VERSION=`sed -n -r 's/version = "(.*)"/\1/p' Cargo.toml | head -n 1`
-
-flatpak-builder --user --install --force-clean "target/flatpak/$VERSION" flatpak.yml
+# Generate deps list for flatpak since flathub doesn't want networking during build
+python3 flatpak-cargo-generator.py Cargo.lock
+# Do the actual build & install locally for testing
+flatpak-builder --user --install --force-clean "target/flatpak/$VERSION" xyz.ljoonal.neospeeps.yml
