ec2ee1a14a95f5d47b082ec93457e1f911f5bb8a
ljoonal <ljoonal@ljoonal.xyz>
2022-08-16 21:59:22 +0300

    Okay so flatpak manifests are stored in a different repo ig

--- build-flatpak.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-# Get current version
-VERSION=`sed -n -r 's/version = "(.*)"/\1/p' Cargo.toml | head -n 1`
-# Generate deps list for flatpak since flathub doesn't want networking during build
-python3 flatpak-cargo-generator.py Cargo.lock
-# Do the actual build & install locally for testing
-flatpak-builder --user --install --force-clean "target/flatpak/$VERSION" xyz.ljoonal.neospeeps.yml
